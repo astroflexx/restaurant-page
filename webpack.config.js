c737402e2963d2352b97c5fa9a7c891ceee66624
Adithya V <venkateshadithya007@gmail.com>
2024-05-07 00:05:55 +0530

    Remove unnecessary code from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -37,9 +37,4 @@ module.exports = {
     optimization: {
         runtimeChunk: 'single',
     },
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    },
 };
